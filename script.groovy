094f88a780f9beeff1e0c4c9f9ba2aceb98f4dd7
Neil Everette <neileverette@users.noreply.github.com>
2024-01-16 20:53:52 -0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
 def buildJar() {
-    echo 'building the application...'
+    echo 'building the jar file'
     sh 'mvn package'
 }
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+    echo 'building the image'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable:'USER')]){
+        sh 'docker build -t neileverette/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push neileverette/demo-app:jma-2.0'
     }
 }
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo 'deploying the app'
 }
 
 return this
