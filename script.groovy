8ad85f987d76614f073736a5fc303d80aa841719
Neil Everette <neil.everette@gmail.com>
2024-02-03 08:56:33 -0600

    editing script file

--- script.groovy
+++ script.groovy
@@ -1,19 +1,23 @@
-def buildJar() {
-    echo 'building the jar file'
+def buildJar(){
+    echo "Building the application"
     sh 'mvn package'
 }
 
-def buildImage() {
-    echo 'building the image'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable:'USER')]){
-        sh 'docker build -t neileverette/demo-app:jma-2.0 .'
+def buildImage(){
+    echo "building image"
+    withCredentials([usernamePassword(credentialsId:'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t neileverette/demo-app:jma-2.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push neileverette/demo-app:jma-2.0'
-    }
+        sh 'docker push neileverette/demo-app:jma-2.1'
+        }
 }
 
-def deployApp() {
-    echo 'deploying the app'
+def deployApp(){
+    echo "Deploying the application"
+}
+
+def cleanApp(){
+    echo "Cleaning things up"
 }
 
 return this
