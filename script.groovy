419be759f318ec3cc144d2a361f65a89d8ca850a
Neil Everette <neil.everette@gmail.com>
2024-02-05 07:47:42 -0600

    bug fix on deploy

--- script.groovy
+++ script.groovy
@@ -16,7 +16,7 @@ def deployApp(){
     echo '-------------------------'  
     echo 'deploying the app to EC2' 
     echo '-------------------------'   
-    def dockerCMD = 'docker run -p 3080:3080 -d neileverette/demo-app:2.1'
+    def dockerCMD = 'docker run -p 3080:3080 -d neileverette/demo-app:jma-2.1'
     sshagent(['ec2-server-key']) {
         sh "ssh -o StrictHostKeyChecking=no ec2-user@18.222.122.196 ${dockerCMD}"
 }
