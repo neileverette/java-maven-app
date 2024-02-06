14a4c5ded47918127fe722c133c928cc42bdb6a4
Neil Everette <neil.everette@gmail.com>
2024-02-05 20:29:54 -0600

    Getting ready to mess shit up

--- script.groovy
+++ script.groovy
@@ -16,11 +16,10 @@ def deployApp(){
     echo '-------------------------'  
     echo 'deploying the app to EC2' 
     echo '-------------------------'   
-//    def dockerCMD = 'docker run -p 3080:3080 -d neileverette/demo-app:jma-2.1'
-//    sshagent(['ec2-server-key']) {
-//        sh "ssh -o StrictHostKeyChecking=no ec2-user@18.222.122.196 ${dockerCMD}"
-//        }
-
+    def dockerCMD = 'docker run -p 3080:3080 -d neileverette/demo-app:jma-2.1'
+    sshagent(['ec2-server-key']) {
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@18.222.122.196 ${dockerCMD}"
+        }
 }
 
 def cleanApp(){
