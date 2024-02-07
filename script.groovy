f94b6dbdad91d310e9bdf152350a7ad42b4f348f
Neil Everette <neil.everette@gmail.com>
2024-02-07 06:47:32 -0600

    added docker-compose.yaml command to deploy step

--- script.groovy
+++ script.groovy
@@ -16,11 +16,26 @@ def deployApp(){
     echo '-------------------------'  
     echo 'deploying the app to EC2' 
     echo '-------------------------'   
-    //def dockerCMD = 'docker run -p 3080:3080 -d neileverette/demo-app:jma-2.1'
-    def dockerComposeCmd = 'docker-compose -f docker-compose.yaml up --detach'
+    
+    // define docker compose command 
+    //def dockerComposeCmd = 'docker-compose -f docker-compose.yaml up --detach'
+    
+    // define script to run on server
+    def shellCmd = 'bash ./server-commands.sh'
+    
     sshagent(['ec2-server-key']) {
+        
+        // copy server script file to serrver
+        sh 'scp server-commands.sh ec2-user@18.222.122.196:/home/ec2-user'
+        
+        // copy docker compose file to server
         sh 'scp docker-compose.yaml ec2-user@18.222.122.196:/home/ec2-user'
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@18.222.122.196 ${dockerComposeCmd}"
+
+        // run shell commond on server
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@18.222.122.196 ${shellCmd}"
+        
+        // run docker compose file on server
+        //sh "ssh -o StrictHostKeyChecking=no ec2-user@18.222.122.196 ${dockerComposeCmd}"
 }
 
 }
